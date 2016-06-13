ffbd98c84035e8442dcc9acf7184febe838c840d
Nyingi Maina <nyingi.maina@gmail.com>
2016-06-13 22:06:17 +0300

    Add a license file

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,31 +1,27 @@
-﻿#region Using directives
-
+#region Using directives
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
 #endregion
-
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Jattac.Libs.WinFormsBinding")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Jattac.Libs.WinFormsBinding")]
-[assembly: AssemblyCopyright("Copyright 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
+[assembly: AssemblyTitle ("DashMVVM")]
+[assembly: AssemblyDescription ("Inspired by MvvmCross for Xamarin.Android and Xamarin.iOS, this is a basic Mvvm library for WinForms that seeks to channel the ease, structure and clarity that simple, robust and predictable databinding brings to apps.")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Nyingi Maina")]
+[assembly: AssemblyProduct ("DashMVVM")]
+[assembly: AssemblyCopyright ("Copyright 2016")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
-[assembly: ComVisible(false)]
-
+[assembly: ComVisible (false)]
 // The assembly version has following format :
 //
 // Major.Minor.Build.Revision
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion ("1.0.*")]
