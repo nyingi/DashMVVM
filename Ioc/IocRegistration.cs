db563b6b96b422390e0ab685ef74d4f673c769e9
Nyingi Maina <nyingimaina@gmail.com>
2016-08-19 04:33:42 +0300

    Added dependancy injection functionality

---
+++ Ioc/IocRegistration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DashMvvm.Ioc
+{
+    internal class IocRegistration
+    {
+        public bool IsSingleton { get; set; }
+
+        public Dictionary<Type, object> ResolutionExpression { get; set; }
+
+        public object NewestInstance { get; set; }
+        
+
+
+    }
+}
